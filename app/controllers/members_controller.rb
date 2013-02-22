c29bd65c3ec5edb857996e298e16887bf9b68a80
Leandro Marcucci <leanucci@gmail.com>
2013-02-22 19:29:00 -0300

    Search member types by title not name

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -6,9 +6,9 @@ class MembersController < ApplicationController
   def index
     if params[:year_id].present?
 
-      @pillars = Member.joins([:year, :member_type]).where("years.id = '#{params[:year_id]}' AND member_types.name = 'Pillar'").sort_by(&:get_last_name)
-      @benefactors = Member.joins([:year, :member_type]).where("years.id = '#{params[:year_id]}' AND member_types.name = 'Benefactor'").sort_by(&:get_last_name)
-      @patrons = Member.joins([:year, :member_type]).where("years.id = '#{params[:year_id]}' AND member_types.name = 'Patron'").sort_by(&:get_last_name)
+      @pillars = Member.joins([:year, :member_type]).where("years.id = '#{params[:year_id]}' AND member_types.title = 'Pillar'").sort_by(&:get_last_name)
+      @benefactors = Member.joins([:year, :member_type]).where("years.id = '#{params[:year_id]}' AND member_types.title = 'Benefactor'").sort_by(&:get_last_name)
+      @patrons = Member.joins([:year, :member_type]).where("years.id = '#{params[:year_id]}' AND member_types.title = 'Patron'").sort_by(&:get_last_name)
 
       @members = @patrons + @benefactors + @patrons
 
