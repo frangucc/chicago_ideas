b97c0283dfa8d0ca6d2284230eba48863f9ff180
Martin Rodriguez <merodriguezblanco@gmail.com>
2013-02-25 12:10:13 -0200

    Tests for cooperative application mailers.

--- spec/factories/cooperative_applications.rb
+++ spec/factories/cooperative_applications.rb
@@ -2,5 +2,25 @@
 
 FactoryGirl.define do
   factory :cooperative_application do
+    sequence(:id)      { |n| n }
+    name               'John'
+    last_name          'Doe'
+    title              'Title'
+    organization       'Organization'
+    phone              '123-4567'
+    org_mission        'Organization Mission'
+    org_twitter        'johndoe'
+    org_website        'www.organization.com'
+    reason             'Reason'
+    worked_on          'Worked On'
+    part_meaningful    'Meaningful Part'
+    ins_failure        'Failure instance'
+    neighborhood       'Neighborhood'
+    assisted_area      'Assisted Area'
+    recommend          'Recommendations'
+    email              'johndoe@domain.com'
+    passion_1          'passion1'
+    passion_2          'passion2'
+    passion_3          'passion3'
   end
 end
---
+++ spec/mailers/cooperative_mailer.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+describe CooperativeMailer do
+
+  let(:co_app) { FactoryGirl.build(:cooperative_application) }
+
+  describe 'send form mail' do
+
+    before(:each) do
+      CooperativeMailer.send_form(co_app).deliver
+      @email = ActionMailer::Base.deliveries.last
+    end
+
+    it 'should have proper headers' do
+      @email.subject.should == 'Cooperative Application Form Submission'
+      @email.from[0].should == 'kelly@chicagoideas.com'
+      @email.to[0].should   == 'kelly@chicagoideas.com'
+    end
+
+    it 'should have proper content' do
+      @email.body.should match(/Cooperative Application Information\./)
+      @email.body.should have_link('Application', :href => admin_cooperative_application_path(co_app))
+    end
+
+  end
+
+  describe 'thank you application mail' do
+
+    before(:each) do
+      CooperativeMailer.thank_you_application(co_app).deliver
+      @email = ActionMailer::Base.deliveries.last
+    end
+
+    it 'should have proper headers' do
+      @email.subject.should == 'Cooperative Application Form Submission'
+      @email.from[0].should == 'kelly@chicagoideas.com'
+      @email.to[0].should   == co_app.email
+    end
+
+    it 'should have proper content' do
+      @email.body.should match(/We Just Want To Say Thank You\!/)
+      @email.body.should match(/Hi, #{co_app.name},/)
+      @email.body.should match(/Thanks for applying to the Co-op\! On behalf of everybody at CIW, we appreciate your interest and ethusiasm - we couldn't do this without you\!/)
+      @email.body.should match(/We'll be in touch within the next few weeks\. In the meantime, feel free to connect with us on Facebook and Twitter\!/)
+      @email.body.should have_link('kelly@chicagoideas.com')
+      @email.body.should match(/312-765-7819/)
+    end
+
+  end
+
+end
