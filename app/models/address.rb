9548ea1181c2abe46dcef5e3814da8078ec9ab8e
Leandro Marcucci <leanucci@gmail.com>
2013-03-01 16:35:43 -0300

    Minimal validations for address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,9 @@
 class Address < ActiveRecord::Base
+
+  validates :street_1, presence: true
+  validates :city, presence: true
+  validates :zip, presence: true
+  validates :country, presence: true
+  validates :state, presence: true
+
 end
