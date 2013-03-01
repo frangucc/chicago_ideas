485b853a694b9ecc0d9f714cce4ab6d4e772281c
Leandro Marcucci <leanucci@gmail.com>
2013-02-28 22:15:56 -0300

    Created Addresses

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130227171255) do
+ActiveRecord::Schema.define(:version => 20130301011340) do
+
+  create_table "addresses", :force => true do |t|
+    t.string   "street_1"
+    t.string   "street_2"
+    t.string   "city"
+    t.string   "zip"
+    t.string   "country"
+    t.string   "state"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "affiliate_event_applications", :force => true do |t|
     t.string   "first_name",           :null => false
