72f3deaef084d4b2b2dc63e866410b2952831205
Martin Rodriguez <merodriguezblanco@gmail.com>
2013-03-04 11:12:45 -0200

    Fixed labs application spreagsheet URL.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
   CIW_LINKEDIN_URL     = 'www.linkedin.com/company/chicago-ideas-week'
   CIW_GPLUS_URL        = 'https://plus.google.com/113007296557109921813/posts'
   CIW_BLOG_URL         = 'http://blog.chicagoideas.com/'
-  LABS_DOC_URL         = 'https://docs.google.com/spreadsheet'
+  LABS_DOC_URL         = 'https://docs.google.com/spreadsheet/viewform?formkey=dG5EZTFJTV81Y1VUdFFHeGp6V3hsdUE6MQ'
 
   # EMAILS
   CIW_MEMBERSHIP_EMAIL = 'membership@chicagoideas.com'
