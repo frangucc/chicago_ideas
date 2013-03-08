e7b64b5d67e3b944deb668c7b7f390383d7140ff
Leandro Marcucci <leanucci@gmail.com>
2013-03-08 16:23:55 -0300

    Display orders in the admin

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,4 +2,8 @@ class Admin::OrdersController < Admin::AdminController
   def index
     @orders = Order.includes(:member_type, :user).order('created_at DESC').page(params[:page])
   end
+
+  def show
+    @order = Order.includes(:member_type, :billing_address, :user => [:member, :address] ).find_by_code(params[:id])
+  end
 end
