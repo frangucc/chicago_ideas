936be64af24cf2e07b87ca8fe0dff21472db12cc
Martin Rodriguez <merodriguezblanco@gmail.com>
2013-03-11 14:47:25 -0300

    Added BING_MAPS_API_KEY to environment.rb.

--- config/environment.rb
+++ config/environment.rb
@@ -16,3 +16,4 @@ DocRaptor.api_key 'tl8fHT64wxhkLUTW0Y6V'
 AUTHNET_LOGIN = "2y9kBc3rXn"
 AUTHNET_KEY = "8SXA34Ag6qa28SuH"
 AUTHNET_ENV = :sandbox
+BING_MAPS_API_KEY = "AuGgGandaCq-kaRoSzngT0VCwi8MWNk0BgOj5pxdiJvooLyZrGwoNnFpT3ZRBMTi"
