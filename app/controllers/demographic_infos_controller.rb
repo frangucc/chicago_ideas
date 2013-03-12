fbe26daad643a11c2414fe50e0b60841229e37cb
Leandro Marcucci <leanucci@gmail.com>
2013-03-12 02:20:01 -0300

    Create DemographicInfo when at least one answer present

--- app/controllers/demographic_infos_controller.rb
+++ app/controllers/demographic_infos_controller.rb
@@ -2,7 +2,9 @@ class DemographicInfosController < ApplicationController
 
   def create
     user = Order.find(params[:order_id]).user
-    user.create_demographic_info(params[:demographic_info])
+    if params[:demographic_info].keys[0..-2].join.length > 0
+      user.create_demographic_info(params[:demographic_info])
+    end
     respond_to do |format|
       format.js { render nothing: true, status: :ok }
     end
