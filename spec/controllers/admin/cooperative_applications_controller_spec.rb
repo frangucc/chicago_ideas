cf1efbaa0df988f21d73ccfacdfd7e3dcb1bb9aa
Martin Rodriguez <merodriguezblanco@gmail.com>
2013-03-14 16:30:03 -0300

    Tests for admin coop and bhsi app controllers.

---
+++ spec/controllers/admin/cooperative_applications_controller_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe Admin::CooperativeApplicationsController do
+
+  before(:each) do
+    Year.create(:id => 2013)
+    Year.stub(:find).and_return(double(:year))
+    TalkBrand.stub_chain(:find, :talks, :current, :order, :limit).and_return([])
+    @user = FactoryGirl.create(:user)
+    sign_in @user
+  end
+
+  after(:each) do
+    sign_out @user
+  end
+
+  describe '#index' do
+
+    ['html', 'xls'].each do |format|
+      context "when format is #{format}" do
+        it 'should redirect' do
+          get :index, :format => format
+          response.should be_redirect
+        end
+      end
+    end
+
+  end
+
+end
