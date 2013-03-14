1b6aa3ea4746b91437c396c959fb016a7b1728f8
Leandro Marcucci <leanucci@gmail.com>
2013-03-14 13:48:30 -0300

    New way to load credentials

--- config/environment.rb
+++ config/environment.rb
@@ -13,9 +13,14 @@ CraigsAdmin::Application.initialize!
 DocRaptor.api_key 'tl8fHT64wxhkLUTW0Y6V'
 
 
-@auth_net = YAML.load_file(Rails.root + 'config/authorize_net.yml')
+if File.exists?(Rails.root + 'config/authorize_net.yml')
+  @auth_net = YAML.load_file(Rails.root + 'config/authorize_net.yml')
+else
+  @auth_net = { "login" => "2y9kBc3rXn", "key" => "8SXA34Ag6qa28SuH", "env" => :staging }
+end
 
 AUTHNET_LOGIN = @auth_net["login"]
 AUTHNET_KEY   = @auth_net["key"]
 AUTHNET_ENV   = @auth_net["env"].to_sym
+
 BING_MAPS_API_KEY = "AuGgGandaCq-kaRoSzngT0VCwi8MWNk0BgOj5pxdiJvooLyZrGwoNnFpT3ZRBMTi"
