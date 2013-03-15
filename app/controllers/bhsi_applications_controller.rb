cb69adc9db66b4492639b757b82990d148d6c5f0
Martin Rodriguez <merodriguezblanco@gmail.com>
2013-03-15 14:21:51 -0300

    WIP: Attach pdf to bhsi application email for applicants.

--- app/controllers/bhsi_applications_controller.rb
+++ app/controllers/bhsi_applications_controller.rb
@@ -33,16 +33,12 @@ class BhsiApplicationsController < ApplicationController
       @meta_data = {:page_title => "Bluhm/Helfand Social Innovation Fellowship", :og_image => "http://www.chicagoideas.com/assets/application/affilliate_events_banner.jpg", :og_title => "Bluhm/Helfand Social Innovation Fellowship | Chicago Ideas Week", :og_type => "website", :og_desc => "Chicago Ideas Week (CIW) is about the sharing of ideas, inspiring action and igniting change to positively impact our world. People who come to CIW are artists, engineers, technologists, inventors, scientists, musicians, economists, explorers-and, well...just innately passionate."}
       @bhsi_application = current_user.build_bhsi_application(params[:bhsi_application])
 
-
-      #pdf = doc_raptor_send({:document_type => "pdf".to_sym})
-      friendlyName = "Bhsi_#{@bhsi_application.social_venture_name}.pdf"
-      friendlyName = friendlyName.gsub(" ", "")
-      friendlyName = friendlyName.gsub("/", "_")
-      #File.open("#{Rails.root}/tmp/#{friendlyName}", 'w+b') {|f| f.write(pdf) }
-      #@bhsi_application.pdf = File.open("#{Rails.root}/tmp/#{friendlyName}");
+      html_file = render_to_string('bhsi_applications/_bhsi_application_pdf.pdf.haml', :layout => false)
+      kit = PDFKit.new(html_file, :page_size => 'Letter')
+      @bhsi_application.pdf = kit.to_file("#{Rails.root}/tmp/#{generate_pdf_name(@bhsi_application)}")
 
       if @bhsi_application.save
-        BhsiApplicationsMailer.send_form(params[:bhsi_application], friendlyName).deliver
+        BhsiApplicationsMailer.send_form(@bhsi_application, generate_pdf_name(@bhsi_application)).deliver
         BhsiApplicationsMailer.thank_you_application(@bhsi_application).deliver
         render 'application/confirmation', :locals => {:title => "BHSI Application Confirmation", :body => "Thank you for applying to the Bluhm/Helfand Social Innovation Fellowship. BHSI semi-finalists will be announced in mid-June.", :url => "http://bit.ly/wdTJfn", :share_text => "I applied to the #BHSI Fellowship at @chicagoideas! RT to all #innovative #socent! Applications close 5/21. Apply today: http://bit.ly/wdTJfn"}
 
@@ -56,6 +52,15 @@ class BhsiApplicationsController < ApplicationController
       redirect_to root_path
     end
 
+  end
+
+  private
+
+  def generate_pdf_name(bhsi_application)
+    pdf_name = "Bhsi_#{bhsi_application.social_venture_name}.pdf"
+    pdf_name.gsub!(' ', '')
+    pdf_name.gsub!('/', '_')
+    pdf_name
   end
 
 end
