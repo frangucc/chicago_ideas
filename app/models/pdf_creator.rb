ab91e6393a37939393c27dee153e07256069d6d7
Leandro Marcucci <leanucci@gmail.com>
2013-03-16 21:02:24 -0300

    Wip: Refactor this

--- app/models/pdf_creator.rb
+++ app/models/pdf_creator.rb
@@ -1,6 +1,7 @@
 module PdfCreator
   def create_pdf(partial_path, css_name, file_name, pdf_kit_options={})
-    html = render_to_string(partial: partial_path, layout: false)
+
+    html = ActionController::Base.render_to_string(partial: partial_path, layout: false)
     pdf_kit_default_options = { "margin-bottom" => "0.5in", "margin-top" => "0.5in", page_size: "Letter" }
     pdf_kit_default_options.merge!(pdf_kit_options)
     kit = PDFKit.new(html, pdf_kit_default_options)
@@ -9,7 +10,7 @@ module PdfCreator
                 else
                   "#{Rails.root}/app/assets/stylesheets/"
                 end
-    kit.stylesheets << css_path + css_name
+    kit.stylesheets << css_path + css_name if css_name
     file = kit.to_file("#{Rails.root}/tmp/pdf/#{file_name}.pdf")
     file
   end
