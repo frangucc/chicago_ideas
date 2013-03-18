e947540f0b84dc28a226a76f372be73493ed92a8
Leandro Marcucci <leanucci@gmail.com>
2013-03-18 03:05:12 -0300

    PdfCreator does not use custom css

--- app/models/pdf_creator.rb
+++ app/models/pdf_creator.rb
@@ -1,16 +1,19 @@
 module PdfCreator
-  def create_pdf(partial_path, css_name, file_name, pdf_kit_options={})
+  def create_pdf(obj, partial_path, file_name, pdf_kit_options={})
 
-    html = ActionController::Base.render_to_string(partial: partial_path, layout: false)
+    template = File.read("#{Rails.root}/app/views/#{ partial_path }")
+    engine = Haml::Engine.new(template)
+    html = engine.render(obj)
+    # File.open("#{Rails.root}/tmp/pdf/#{file_name}.html", 'w') {|f| f.write(html) }
     pdf_kit_default_options = { "margin-bottom" => "0.5in", "margin-top" => "0.5in", page_size: "Letter" }
     pdf_kit_default_options.merge!(pdf_kit_options)
     kit = PDFKit.new(html, pdf_kit_default_options)
-    css_path =  if Rails.env == "production"
-                  "#{Rails.root}/public/assets/"
-                else
-                  "#{Rails.root}/app/assets/stylesheets/"
-                end
-    kit.stylesheets << css_path + css_name if css_name
+    # css_path =  if Rails.env == "production"
+    #               "#{Rails.root}/public/assets/"
+    #             else
+    #               "#{Rails.root}/app/assets/stylesheets/"
+    #             end
+    # kit.stylesheets << css_path + css_name if css_name
     file = kit.to_file("#{Rails.root}/tmp/pdf/#{file_name}.pdf")
     file
   end
