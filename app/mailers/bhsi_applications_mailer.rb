6ba25ff3f88f9ed097aa3b411a0651ced1216563
Leandro Marcucci <leanucci@gmail.com>
2013-03-18 04:53:12 -0300

    Send attachments in bhsi applications

--- app/mailers/bhsi_applications_mailer.rb
+++ app/mailers/bhsi_applications_mailer.rb
@@ -1,15 +1,21 @@
 class BhsiApplicationsMailer < ActionMailer::Base
 
-  default :from    => ApplicationHelper::CIW_BHSI_SUBM_EMAIL
-  default :subject => 'BHSI Application Form Submission'
+  default from:      "test@example.com"
+  default subject:   'BHSI Application Form Submission'
 
-  def send_form(bhsi_application, filename)
-    attachments[filename] = File.read(bhsi_application.pdf)
-    mail(:to => "#{ApplicationHelper::CIW_JESSICA_EMAIL}, #{ApplicationHelper::CIW_COREY_EMAIL}, #{ApplicationHelper::DAVID_EMAIL}")
+  def send_form(bhsi)
+    @bhsi = bhsi
+    attachments[bhsi.pdf_file_name]              = open(bhsi.pdf.url).read
+    attachments[bhsi.previous_budget_file_name]  = open(bhsi.previous_budget.url).read
+    attachments[bhsi.press_clipping_1_file_name] = open(bhsi.press_clipping_1.url).read if bhsi.press_clipping_1.present?
+    attachments[bhsi.press_clipping_2_file_name] = open(bhsi.press_clipping_2.url).read if bhsi.press_clipping_2.present?
+    attachments[bhsi.press_clipping_3_file_name] = open(bhsi.press_clipping_3.url).read if bhsi.press_clipping_3.present?
+
+    mail(to: 'leanucci@gmail.com')
   end
 
-  def thank_you_application(bhsi_application)
-    mail(:to => bhsi_application.email)
+  def thank_you_application(bhsi)
+    mail(to: bhsi.email)
   end
 
 end
