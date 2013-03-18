c0cea08989c00fdccfe5c94487edfbaaa704a5b3
Leandro Marcucci <leanucci@gmail.com>
2013-03-18 14:50:23 -0300

    Only attach pdf.

--- app/mailers/bhsi_applications_mailer.rb
+++ app/mailers/bhsi_applications_mailer.rb
@@ -5,11 +5,11 @@ class BhsiApplicationsMailer < ActionMailer::Base
 
   def send_form(bhsi)
     @bhsi = bhsi
-    attachments[bhsi.pdf_file_name]              = open(bhsi.pdf.url).read
-    attachments[bhsi.previous_budget_file_name]  = open(bhsi.previous_budget.url).read
-    attachments[bhsi.press_clipping_1_file_name] = open(bhsi.press_clipping_1.url).read if bhsi.press_clipping_1.present?
-    attachments[bhsi.press_clipping_2_file_name] = open(bhsi.press_clipping_2.url).read if bhsi.press_clipping_2.present?
-    attachments[bhsi.press_clipping_3_file_name] = open(bhsi.press_clipping_3.url).read if bhsi.press_clipping_3.present?
+    attachments[bhsi.pdf_file_name]              = File.read("#{Rails.root}/tmp/pdf/#{bhsi.pdf_file_name}")
+    # attachments[bhsi.previous_budget_file_name]  = open(bhsi.previous_budget.url).read
+    # attachments[bhsi.press_clipping_1_file_name] = open(bhsi.press_clipping_1.url).read if bhsi.press_clipping_1.present?
+    # attachments[bhsi.press_clipping_2_file_name] = open(bhsi.press_clipping_2.url).read if bhsi.press_clipping_2.present?
+    # attachments[bhsi.press_clipping_3_file_name] = open(bhsi.press_clipping_3.url).read if bhsi.press_clipping_3.present?
 
     mail(to: ApplicationHelper::BHSI_RECIPIENTS)
   end
