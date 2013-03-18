45938bda93db95ce14e4dfc5ec3a06c54f8f9862
Martin Rodriguez <merodriguezblanco@gmail.com>
2013-03-18 10:47:59 -0300

    Pdf generation and download link in admin section for cooperative apps.

--- app/controllers/admin/cooperative_applications_controller.rb
+++ app/controllers/admin/cooperative_applications_controller.rb
@@ -9,28 +9,24 @@ class Admin::CooperativeApplicationsController < Admin::AdminController
   def show
     @section_title = 'Detail'
     @cooperative_application = CooperativeApplication.find(params[:id])
+
     respond_to do |format|
-
-      format.pdf {
-
-        # if !@cooperative_application.pdf.exists?
-          pdf = doc_raptor_send({:document_type => "pdf".to_sym})
-          friendlyName = "CPA_#{@cooperative_application.name}.pdf"
-          friendlyName = friendlyName.gsub(" ", "")
-          friendlyName = friendlyName.gsub("/", "_")
-          File.open("#{Rails.root}/tmp/#{friendlyName}", 'w+b') {|f| f.write(pdf) }
-          @cooperative_application.pdf = File.open("#{Rails.root}/tmp/#{friendlyName}");
-          @cooperative_application.save!({:validate => false})
-          send_data pdf, :filename => friendlyName, :type => "pdf"
-        # else
-          # redirect_to @cooperative_application.pdf.url
-        # end
-
-      }
-      format.html {
-        render
-      }
+      format.pdf do
+        html_file = render_to_string(:partial => 'cooperative/cooperative_application_pdf', :layout => false)
+        kit = PDFKit.new(html_file, :page_size => 'Letter')
+        send_data kit.to_pdf, :filename => generate_pdf_name(@cooperative_application), :type => 'application/pdf'
+      end
+      format.html {}
     end
+  end
+
+  private
+
+  def generate_pdf_name(cooperative_application)
+    pdf_name = "CPA_#{cooperative_application.name}_#{cooperative_application.last_name}.pdf"
+    pdf_name.gsub!(' ', '')
+    pdf_name.gsub!('/', '_')
+    pdf_name
   end
 
 end
