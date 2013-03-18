a7507ee3f10a9306c1d1d74fd65f23fb51fed283
Leandro Marcucci <leanucci@gmail.com>
2013-03-18 03:05:30 -0300

    Never use Github.app to commit

--- app/controllers/bhsi_applications_controller.rb
+++ app/controllers/bhsi_applications_controller.rb
@@ -27,10 +27,7 @@ class BhsiApplicationsController < ApplicationController
   end
 
   def create
-    # Prevent duplicate submissions
-    if current_user and current_user.bhsi_application.blank?
-
-      @meta_data = {:page_title => "Bluhm/Helfand Social Innovation Fellowship", :og_image => "http://www.chicagoideas.com/assets/application/affilliate_events_banner.jpg", :og_title => "Bluhm/Helfand Social Innovation Fellowship | Chicago Ideas Week", :og_type => "website", :og_desc => "Chicago Ideas Week (CIW) is about the sharing of ideas, inspiring action and igniting change to positively impact our world. People who come to CIW are artists, engineers, technologists, inventors, scientists, musicians, economists, explorers-and, well...just innately passionate."}
+    @meta_data = {:page_title => "Bluhm/Helfand Social Innovation Fellowship", :og_image => "http://www.chicagoideas.com/assets/application/affilliate_events_banner.jpg", :og_title => "Bluhm/Helfand Social Innovation Fellowship | Chicago Ideas Week", :og_type => "website", :og_desc => "Chicago Ideas Week (CIW) is about the sharing of ideas, inspiring action and igniting change to positively impact our world. People who come to CIW are artists, engineers, technologists, inventors, scientists, musicians, economists, explorers-and, well...just innately passionate."}
     if current_user && current_user.bhsi_application.blank?
       @bhsi_application = current_user.build_bhsi_application(params[:bhsi_application])
 
