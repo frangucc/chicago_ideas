5ef602656ef5775bf1e13c3bcc516fcc0bd5d472
Leandro Marcucci <leanucci@gmail.com>
2013-03-18 14:50:33 -0300

    We dont require this field for now

--- app/models/bhsi_application.rb
+++ app/models/bhsi_application.rb
@@ -52,7 +52,7 @@ class BhsiApplication < ActiveRecord::Base
   validates :reference_2_phone,         :presence => true
   validates :reference_2_email,         :presence => true
   validates :agreement_accepeted,       :acceptance => {:accept => true}
-  validates :org_founder,               :presence => true
+  # validates :org_founder,               :presence => true
   validates :total_budget_current_year, :presence => true
   validates :budget_previous_year,      :presence => true
   validates :budget_current_year,       :presence => true
