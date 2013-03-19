bce4b6a1b06ad6fac4b26b08cc38b101c66c8f8d
Leandro Marcucci <leanucci@gmail.com>
2013-03-19 01:36:47 -0300

    Fixed typo on migration

--- db/migrate/20130319013029_bhsi_applications_is_org_founder.rb
+++ db/migrate/20130319013029_bhsi_applications_is_org_founder.rb
@@ -1,6 +1,6 @@
 class BhsiApplicationsIsOrgFounder < ActiveRecord::Migration
   def change
-    remove_column :bhsi_applications, :org_founder, :boolean
+    remove_column :bhsi_applications, :org_founder
     add_column :bhsi_applications, :org_founder, :boolean
   end
 end
