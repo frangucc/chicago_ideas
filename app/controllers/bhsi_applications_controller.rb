21b63ba6f546449de0ea33950555ebc88338f334
Leandro Marcucci <leanucci@gmail.com>
2013-03-19 01:33:49 -0300

    Bhsi application pdf generation

--- app/controllers/bhsi_applications_controller.rb
+++ app/controllers/bhsi_applications_controller.rb
@@ -15,9 +15,10 @@ class BhsiApplicationsController < ApplicationController
 
   def create
     @meta_data = {:page_title => "Bluhm/Helfand Social Innovation Fellowship", :og_image => "http://www.chicagoideas.com/assets/application/affilliate_events_banner.jpg", :og_title => "Bluhm/Helfand Social Innovation Fellowship | Chicago Ideas Week", :og_type => "website", :og_desc => "Chicago Ideas Week (CIW) is about the sharing of ideas, inspiring action and igniting change to positively impact our world. People who come to CIW are artists, engineers, technologists, inventors, scientists, musicians, economists, explorers-and, well...just innately passionate."}
-    @bhsi_application = BhsiApplication.new(params[:bhsi_application])
 
-    if @bhsi_application.save
+    @bhsi_application = BhsiApplication.new(params[:bhsi_application])
+    @bhsi_application.html2pdf = render_to_string(template: "bhsi_applications/bhsi_pdf.html.haml", layout: false)
+    if @bhsi_application.save!
       render 'application/confirmation', :locals => {:title => "BHSI Application Confirmation", :body => "Thank you for applying to the Bluhm/Helfand Social Innovation Fellowship. BHSI semi-finalists will be announced in mid-June.", :url => "http://bit.ly/wdTJfn", :share_text => "I applied to the #BHSI Fellowship at @chicagoideas! RT to all #innovative #socent! Applications close 5/21. Apply today: http://bit.ly/wdTJfn"}
     else
       flash[:notice] = 'Please fill in all required fields!'
