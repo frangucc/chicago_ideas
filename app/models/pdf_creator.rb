c9f02d2e784dbdb487e04f689029de5499c5aafe
Leandro Marcucci <leanucci@gmail.com>
2013-03-20 16:35:50 -0300

    Fixes to download BHSI apps pdf in the admin

--- app/models/pdf_creator.rb
+++ app/models/pdf_creator.rb
@@ -8,12 +8,12 @@ module PdfCreator
     pdf_kit_default_options = { "margin-bottom" => "0.5in", "margin-top" => "0.5in", page_size: "Letter" }
     pdf_kit_default_options.merge!(pdf_kit_options)
     kit = PDFKit.new(html, pdf_kit_default_options)
-    # css_path =  if Rails.env == "production"
-    #               "#{Rails.root}/public/assets/"
-    #             else
-    #               "#{Rails.root}/app/assets/stylesheets/"
-    #             end
-    # kit.stylesheets << css_path + css_name if css_name
+    css_path =  if Rails.env == "development"
+                  "#{Rails.root}/app/assets/stylesheets/"
+                else
+                  "#{Rails.root}/public/assets/"
+                end
+    kit.stylesheets << css_path + css_name if css_name
     file = kit.to_file("#{Rails.root}/tmp/pdf/#{file_name}")
     file
   end
