a60991567f89c3c819d8ef1c8211e23ce63f6f31
Leandro Marcucci <leanucci@gmail.com>
2013-03-21 02:28:40 -0300

    Load attachments from filesystem

--- app/mailers/bhsi_applications_mailer.rb
+++ app/mailers/bhsi_applications_mailer.rb
@@ -7,12 +7,12 @@ class BhsiApplicationsMailer < ActionMailer::Base
     @bhsi = bhsi
     attachments[bhsi.pdf_file_name] = File.read("/tmp/#{ @bhsi.pdf_file_name }")
 
-    attachments[bhsi.previous_budget_file_name]       = open(bhsi.previous_budget.url).read if bhsi.previous_budget.present?
-    attachments[bhsi.current_budget_file_name]        = open(bhsi.current_budget.url).read if bhsi.current_budget.present?
-    attachments[bhsi.venture_standard_deck_file_name] = open(bhsi.venture_standard_deck.url).read if bhsi.venture_standard_deck.present?
-    attachments[bhsi.press_clipping_1_file_name]      = open(bhsi.press_clipping_1.url).read if bhsi.press_clipping_1.present?
-    attachments[bhsi.press_clipping_2_file_name]      = open(bhsi.press_clipping_2.url).read if bhsi.press_clipping_2.present?
-    attachments[bhsi.press_clipping_3_file_name]      = open(bhsi.press_clipping_3.url).read if bhsi.press_clipping_3.present?
+    attachments[bhsi.previous_budget_file_name]       = File.read(bhsi.previous_budget.path)        if bhsi.previous_budget.present?
+    attachments[bhsi.current_budget_file_name]        = File.read(bhsi.current_budget.path)         if bhsi.current_budget.present?
+    attachments[bhsi.venture_standard_deck_file_name] = File.read(bhsi.venture_standard_deck.path)  if bhsi.venture_standard_deck.present?
+    attachments[bhsi.press_clipping_1_file_name]      = File.read(bhsi.press_clipping_1.path)       if bhsi.press_clipping_1.present?
+    attachments[bhsi.press_clipping_2_file_name]      = File.read(bhsi.press_clipping_2.path)       if bhsi.press_clipping_2.present?
+    attachments[bhsi.press_clipping_3_file_name]      = File.read(bhsi.press_clipping_3.path)       if bhsi.press_clipping_3.present?
 
     mail(to: ApplicationHelper::BHSI_RECIPIENTS) do |format|
       format.text { render 'notify_staff'}
