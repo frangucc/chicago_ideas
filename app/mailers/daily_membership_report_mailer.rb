272471f2127a5cdf3dddf9f6f996554282cf75a1
Martin Rodriguez <merodriguezblanco@gmail.com>
2013-03-26 11:41:11 -0300

    Daily report mailer with excel files attached.

---
+++ app/mailers/daily_membership_report_mailer.rb
@@ -0,0 +1,19 @@
+class DailyMembershipReportMailer < ActionMailer::Base
+
+  default :from    => ApplicationHelper::CIW_MEMBERSHIP_EMAIL
+  default :subject => "Daily Membership Report"
+
+  def daily_report
+    Order.generate_members_stats_excel
+    Member.generate_current_year_members_excel
+
+    members_list_file_name  = "Member_list_#{Time.current.year}.csv"
+    members_stats_file_name = "Member_stats_#{Time.current.year}.csv"
+
+    attachments[members_list_file_name]  = File.read("/tmp/#{members_list_file_name}")
+    attachments[members_stats_file_name] = File.read("/tmp/#{members_stats_file_name}")
+
+    mail(:to => ApplicationHelper::BHSI_RECIPIENTS)
+  end
+
+end
---
+++ spec/mailers/daily_membership_report_mailer_spec.rb
@@ -0,0 +1,32 @@
+require "spec_helper"
+
+describe DailyMembershipReportMailer do
+
+  describe '#daily_report' do
+
+    CURRENT_YEAR = Time.current.year
+
+    before do
+      Order.stub(:generate_members_stats_excel)
+      Member.stub(:generate_current_year_members_excel)
+      ["Member_list_#{CURRENT_YEAR}.csv", "Member_stats_#{CURRENT_YEAR}.csv"].each do |file_name|
+        CSV.open("/tmp/#{file_name}", "wb")
+      end
+      DailyMembershipReportMailer.daily_report.deliver
+      @email = ActionMailer::Base.deliveries.last
+    end
+
+    it 'sends proper headers' do
+      @email.subject.should   == 'Daily Membership Report'
+      @email.from[0].should   == 'membership@chicagoideas.com'
+      @email.to.to_set.should == ['leandro@meetmantra.com', 'martin@meetmantra.com'].to_set
+    end
+
+    it 'sends proper attachments' do
+      @email.attachments.count.should == 2
+      @email.attachments.collect { |a| a.content_type }.to_set.should == ["text/csv; filename=Member_list_#{CURRENT_YEAR}.csv", "text/csv; filename=Member_stats_#{CURRENT_YEAR}.csv"].to_set
+    end
+
+  end
+
+end
