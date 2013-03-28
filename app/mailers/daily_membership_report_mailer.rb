bbf025466354f04b5608d8e796102d0928cc3df5
Martin Rodriguez <merodriguezblanco@gmail.com>
2013-03-28 11:41:01 -0300

    Changed daily excel file names to include current date.

--- app/mailers/daily_membership_report_mailer.rb
+++ app/mailers/daily_membership_report_mailer.rb
@@ -9,13 +9,19 @@ class DailyMembershipReportMailer < ActionMailer::Base
     Order.generate_members_stats_excel
     Member.generate_current_year_members_excel
 
-    members_list_file_name  = "Member_list_#{Time.current.year}.csv"
-    members_stats_file_name = "Member_stats_#{Time.current.year}.csv"
+    members_list_file_name  = "Member_list_#{current_time.year}_#{current_time.strftime("%m-%d-%Y")}.csv"
+    members_stats_file_name = "Member_stats_#{current_time.year}_#{current_time.strftime("%m-%d-%Y")}.csv"
 
     attachments[members_list_file_name]  = File.read("/tmp/#{members_list_file_name}")
     attachments[members_stats_file_name] = File.read("/tmp/#{members_stats_file_name}")
 
     mail(:to => ApplicationHelper::DAILY_REPORT_RECIPIENTS)
+  end
+
+  private
+
+  def current_time
+    Time.current
   end
 
 end
