cbf036deb3c71f40dc5e003f1bd5fcab13a1651d
Martin Rodriguez <merodriguezblanco@gmail.com>
2013-04-04 16:50:37 -0300

    Included SearchSortPaginate module in Material model.

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,5 +1,7 @@
 class Material < ActiveRecord::Base
 
+  include SearchSortPaginate
+
   THUMBNAIL_WIDTH  = 271
   THUMBNAIL_HEIGHT = 211
 
@@ -10,5 +12,17 @@ class Material < ActiveRecord::Base
   validates_attachment :document,  :presence => true, :content_type => { :content_type => /^application\/(msword|vnd\.ms-excel|vnd\.ms-powerpoint|pdf)/ }
 
   validates :name, :presence => true, :uniqueness => true
+
+  # a DRY approach to searching lists of these models
+  def self.search_fields parent_model=nil
+    case parent_model.class.name.underscore
+    when 'foo'
+    else
+      [
+        { :name => :search, :as => :string, :fields => [:name], :wildcard => :both },
+        { :name => :created_at, :as => :datetimerange },
+      ]
+    end
+  end
 
 end
