6fad2274ebbeff5a4c02165e2ac09d1ec39fab37
Martin Rodriguez <merodriguezblanco@gmail.com>
2013-04-04 18:24:17 -0300

    Material CRUD controller and views for admin section.

--- app/controllers/admin/materials_controller.rb
+++ app/controllers/admin/materials_controller.rb
@@ -1,2 +1,29 @@
 class Admin::MaterialsController < Admin::AdminController
+
+  # COLLECTION ACTIONS
+  # ---------------------------------------------------------------------------------------------------------
+  def index
+    @materials = Material.search_sort_paginate(params)
+  end
+
+  # MEMBER ACTIONS
+  # ---------------------------------------------------------------------------------------------------------
+
+  # standard CRUD functionality exists in the base AdminController
+
+  # the detail page for this material
+  def show
+    @section_title = 'Detail'
+    @material = Material.find(params[:id])
+  end
+
+  # MEMBER PAGES
+  # ---------------------------------------------------------------------------------------------------------
+
+  # notes associated with this material
+  def notes
+    @event = Material.find(params[:id])
+    @notes = @material.notes.includes(:author).search_sort_paginate(params, :parent => @material)
+  end
+
 end
