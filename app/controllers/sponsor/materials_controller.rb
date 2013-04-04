9cb8530ef7b998018498d9dc6866fdda30919b4f
Martin Rodriguez <merodriguezblanco@gmail.com>
2013-04-04 10:59:43 -0300

    Materials tab index page. Added headline.

--- app/controllers/sponsor/materials_controller.rb
+++ app/controllers/sponsor/materials_controller.rb
@@ -1,2 +1,6 @@
 class Sponsor::MaterialsController < Sponsor::BaseController
+
+  def index
+  end
+
 end
---
+++ spec/controllers/sponsor/materials_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe Sponsor::MaterialsController do
+
+  before do
+    Year.create(:id => 2013)
+    Year.stub(:find).and_return(double(:year))
+    TalkBrand.stub_chain(:find, :talks, :current, :order, :limit).and_return([])
+    @user = FactoryGirl.create(:user, :is_sponsor => true)
+    sign_in @user
+  end
+
+  after do
+    sign_out @user
+  end
+
+  describe '#index' do
+
+    it 'should success' do
+      get 'index'
+      response.should be_success
+    end
+
+  end
+
+end
