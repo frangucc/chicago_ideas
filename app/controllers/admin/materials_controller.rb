338fb1ea01a2e869fd43dc5d6f49b56cd8c497af
Martin Rodriguez <merodriguezblanco@gmail.com>
2013-04-05 12:42:19 -0300

    Goodbye modals for creating and editing materials on admin section!

--- app/controllers/admin/materials_controller.rb
+++ app/controllers/admin/materials_controller.rb
@@ -11,7 +11,50 @@ class Admin::MaterialsController < Admin::AdminController
 
   # standard CRUD functionality exists in the base AdminController
 
-  # the detail page for this material
+  def new
+    @parent = parent_model
+    @model = new_model(default_model)
+  end
+
+  def edit
+    @model = fetch_model
+  end
+
+  def create
+    @parent = parent_model
+    @model = new_model(params[model_name])
+    @model = pre_create(@model)
+
+    if @model.errors.empty? && @model.save
+      if params[model_name][:notes]
+        model_note(@model, params[model_name][:notes][:body])
+      else
+        model_note(@model, "#{@model.class.name.titlecase} was successfully created.")
+      end
+      redirect_to admin_materials_path, :notice => "#{@model.class.name.titlecase} was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def update
+    @parent = parent_model
+    @model = fetch_model
+    @model = pre_update(@model)
+
+    if @model.errors.empty? && @model.update_attributes(params[model_name])
+      if params[model_name][:notes]
+        model_note(@model, params[model_name][:notes][:body])
+      else
+        model_note(@model, "#{@model.class.name.titlecase} was successfully updated.")
+      end
+      redirect_to admin_materials_path, :notice => "#{@model.class.name.titlecase} was successfully updated."
+    else
+      render :edit
+    end
+
+  end
+
   def show
     @section_title = 'Detail'
     @material = Material.find(params[:id])
