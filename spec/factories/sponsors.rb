cc8870c38da6f0b3446b7399ffbfb8458d61772a
Martin Rodriguez <merodriguezblanco@gmail.com>
2013-04-05 17:35:33 -0300

    Edit and Update remote actions for sponsor controller.

--- spec/factories/sponsors.rb
+++ spec/factories/sponsors.rb
@@ -4,6 +4,7 @@ FactoryGirl.define do
     name        "TIME Magazine"
     description ""
     url         "http://www.time.com/time/"
+    locked      true
     association :sponsorship_level, :factory => :sponsorship_level
   end
 end
--- spec/models/sponsor_spec.rb
+++ spec/models/sponsor_spec.rb
@@ -4,7 +4,7 @@ describe Sponsor do
 
   describe '#active?' do
 
-    let(:sponsor) { FactoryGirl.create(:sponsor, :eps_logo_file_name => 'eps_logo.png', :logo_file_name => 'logo.png', :sponsor_agreement_file_name => 'agreement.txt') }
+    let(:sponsor) { FactoryGirl.create(:sponsor, :eps_logo_file_name => 'eps_logo.png', :logo_file_name => 'logo.png') }
 
     context 'has no eps logo file name' do
       it 'returns false' do
@@ -20,16 +20,49 @@ describe Sponsor do
       end
     end
 
-    context 'has no agreement file name' do
-      it 'returns false' do
-        sponsor.sponsor_agreement_file_name = nil
-        sponsor.active?.should be_false
-      end
-    end
-
     context 'has all file names' do
       it 'returns true' do
         sponsor.active?.should be_true
+      end
+    end
+
+  end
+
+  describe '#unlock!' do
+
+    let(:sponsor) { FactoryGirl.create(:sponsor, :locked => true) }
+
+    context 'inactive and locked' do
+      it 'unlocks the sponsor' do
+        sponsor.stub(:active?).and_return(false)
+        sponsor.unlock!
+        sponsor.reload.locked?.should be_false
+      end
+    end
+
+    context 'active and locked' do
+      it 'does not unlock the sponsor' do
+        sponsor.stub(:active?).and_return(true)
+        sponsor.unlock!
+        sponsor.reload.locked?.should be_true
+      end
+    end
+
+    context 'active and unlocked' do
+      it 'does not unlock the sponsor' do
+        sponsor.update_attributes(:locked => false)
+        sponsor.stub(:active?).and_return(true)
+        sponsor.unlock!
+        sponsor.reload.locked?.should be_false
+      end
+    end
+
+    context 'inactive and unlocked' do
+      it 'does not unlock the sponsor' do
+        sponsor.update_attributes(:locked => false)
+        sponsor.stub(:active?).and_return(false)
+        sponsor.unlock!
+        sponsor.reload.locked?.should be_false
       end
     end
 
