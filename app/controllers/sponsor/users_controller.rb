0457627df8324dead4fcfc471174e4793c598388
Martin Rodriguez <merodriguezblanco@gmail.com>
2013-04-08 15:37:21 -0300

    Primary sponsor user can invite new sponsor users.

--- app/controllers/sponsor/users_controller.rb
+++ app/controllers/sponsor/users_controller.rb
@@ -1,6 +1,22 @@
 class Sponsor::UsersController < Sponsor::BaseController
 
   def invite
+    validate_params :user, :name, :email
+    if User.where(:email => params[:user][:email]).present?
+      @errors << 'Already a user of CIW'
+    end
+
+    if @errors.empty?
+      user = User.invite!(:name  => params[:user][:name], :email => params[:user][:email], :is_sponsor => true, :is_admin_created => true)
+      current_user.sponsor.sponsor_users.create(:user_id => user.id)
+      respond_to do |format|
+        format.js { render :nothing => true, :status => :ok }
+      end
+    else
+      respond_to do |format|
+        format.js { render :json => @errors, :status => :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
