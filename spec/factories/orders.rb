87ec443ca4ed845f20f1adc189c42bb09a05eff7
Martin Rodriguez <merodriguezblanco@gmail.com>
2013-04-09 15:24:05 -0300

    Fixed failing model tests.

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,11 +1,12 @@
 FactoryGirl.define do
   factory :order do
-    name_on_card  'John Doe'
-    card_number   123456789
-    expiry_date   2215
-    cvc           123
-    amount        75
-    association   :member_type, :factory => :member_type
-    association   :user,        :factory => :user
+    name_on_card      'John Doe'
+    card_number       123456789
+    expiry_date_month 15
+    expiry_date_year  22
+    cvc               123
+    amount            75
+    association       :member_type, :factory => :member_type
+    association       :user,        :factory => :user
   end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -9,15 +9,15 @@ describe Order do
       new_order.stub(:amount_should_match_member_types).and_return(true)
     end
 
-    [:name_on_card, :card_number, :amount, :expiry_date, :cvc, :member_type_id].each do |attribute|
+    [:name_on_card, :card_number, :cvc, :member_type_id].each do |attribute|
       it "requires #{attribute}" do
         assert_presence new_order, attribute
       end
     end
 
-    it 'requires expiry_date to be a number' do
-      new_order.expiry_date = 'invalid'
-      assert_numericality new_order, :expiry_date
+    it 'requires amount to be a number' do
+      new_order.amount = 'invalid'
+      assert_numericality new_order, :amount
     end
 
     it 'requires cvc to be a number' do
@@ -25,19 +25,11 @@ describe Order do
       assert_numericality new_order, :cvc
     end
 
-    it 'requires expiry_date to have proper length' do
-      [1, 12, 123].each do |exp_date|
-        new_order.expiry_date = exp_date
-        assert_numerical_range new_order, :expiry_date, 4
-      end
-    end
-
     it 'requires cvc to have proper length' do
-      new_order.cvc = 2
-      assert_numerical_range new_order, :cvc, 3
-
-      new_order.cvc = 5
-      assert_numerical_range new_order, :cvc, 3
+      [2, 5].each do |cvc|
+        new_order.cvc = cvc
+        assert_numerical_range new_order, :cvc, 3
+      end
     end
   end
 
