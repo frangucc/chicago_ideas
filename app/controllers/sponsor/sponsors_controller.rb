b0def8f653f14858532c9326cb5c6d6ad81803ff
Martin Rodriguez <merodriguezblanco@gmail.com>
2013-04-09 10:57:57 -0300

    Finished upload logos registration step for sponsor users.

--- app/controllers/sponsor/sponsors_controller.rb
+++ app/controllers/sponsor/sponsors_controller.rb
@@ -1,20 +1,26 @@
 class Sponsor::SponsorsController < Sponsor::BaseController
 
   def edit
-   @sponsor = current_user.sponsor
+    @sponsor = current_user.sponsor
   end
 
   def update
     @sponsor = current_user.sponsor
+    @errors = []
 
-    if @sponsor.update_attributes(params[:sponsor])
-      @sponsor.unlock!
-      respond_to do |format|
-        format.js { render :nothing => true, :status => :ok }
-      end
-    else
-      respond_to do |format|
-        format.js { render :text => @sponsor.errors.full_messages, :status => :unprocessable_entity }
+    respond_to do |format|
+      if @sponsor.update_attributes(params[:sponsor])
+        if @sponsor.active?
+          @sponsor.unlock!
+          format.js { render :nothing => true, :status => :ok }
+        else
+          @errors << 'Both logos should be provided.'
+          format.js { render :json => @errors, :status => :unprocessable_entity }
+        end
+      else
+        @errors << @sponsor.errors.full_messages
+        @errors.flatten!
+        format.js { render :json => @errors, :status => :unprocessable_entity }
       end
     end
   end
