7468e2aff8efd17c7d9d6590d886b3234ca5e3dd
Martin Rodriguez <merodriguezblanco@gmail.com>
2013-04-17 17:54:11 -0300

    New sponsor events population.

---
+++ db/migrate/20130417204900_update_sponsor_event_dates.rb
@@ -0,0 +1,39 @@
+class UpdateSponsorEventDates < ActiveRecord::Migration
+
+  def change
+    SponsorEvent.delete_all
+    SponsorEvent.create!(:name  => 'Maximizing the CIW Sponsor Portal',
+                         :month => 'July',
+                         :day   => '10',
+                         :info  => 'Info Session at Noon')
+    SponsorEvent.create!(:name  => 'Maximizing the CIW Sponsor Portal',
+                         :month => 'July',
+                         :day   => '17',
+                         :info  => 'Info Session at 5:00pm')
+    SponsorEvent.create!(:name  => 'Ticketing Opens for Sponsors',
+                         :month => 'July',
+                         :day   => '29',
+                         :info  => '')
+    SponsorEvent.create!(:name  => 'Sponsors will receive invitations to all evening VIP receptions',
+                         :month => 'July',
+                         :day   => 'Late',
+                         :info  => '')
+    SponsorEvent.create!(:name  => 'Annual Sponsor Soiree',
+                         :month => 'August',
+                         :day   => '1',
+                         :info  => '')
+    SponsorEvent.create!(:name  => 'Ticketing Opens to CIW Members',
+                         :month => 'August',
+                         :day   => '12',
+                         :info  => '')
+    SponsorEvent.create!(:name  => 'Ticketing Opens to General Public',
+                         :month => 'September',
+                         :day   => '3',
+                         :info  => '')
+    SponsorEvent.create!(:name  => 'Menlo (VIP) Passes will be mailed to sponsors',
+                         :month => 'September',
+                         :day   => 'Mid',
+                         :info  => '')
+  end
+
+end
