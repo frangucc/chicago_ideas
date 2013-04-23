5f5766c041953459a0ebe90064de08b3a7615f05
Martin Rodriguez <merodriguezblanco@gmail.com>
2013-04-23 10:54:11 -0300

    Removed check_primary from sponsor user model.

--- app/models/sponsor_user.rb
+++ app/models/sponsor_user.rb
@@ -8,17 +8,8 @@ class SponsorUser < ActiveRecord::Base
   validates_attachment :eps_logo, :presence => true, :content_type => { :content_type => /image\/(jpg|jpeg|png)/ }, :on => :update
   validates_attachment :logo,     :presence => true, :content_type => { :content_type => /image\/(jpg|jpeg|png)/ }, :on => :update
 
-  before_save :check_primary
-
   def logos_uploaded?
     eps_logo.present? && logo.present?
   end
 
-  private
-    def check_primary
-      if self.primary_contact == true
-        SponsorUser.where(sponsor_id: self.sponsor_id, primary_contact: true).update_all(primary_contact: false)
-        self.primary_contact = true
-      end
-    end
 end
