891fe88262ede2bf417db8f3d42be7f078b02d81
Martin Rodriguez <merodriguezblanco@gmail.com>
2013-04-23 10:09:53 -0300

    Updated SponsorsMailer.

--- app/controllers/sponsor/sponsor_users_controller.rb
+++ app/controllers/sponsor/sponsor_users_controller.rb
@@ -8,7 +8,7 @@ class Sponsor::SponsorUsersController < Sponsor::BaseController
 
     respond_to do |format|
       if @sponsor_user.update_attributes(params[:sponsor_user])
-        #SponsorsMailer.notify_logos_upload(@sponsor).deliver
+        SponsorsMailer.notify_logos_upload(@sponsor_user).deliver
         format.js { render :nothing => true, :status => :ok }
       else
         format.js { render :json => @sponsor_user.errors.full_messages, :status => :unprocessable_entity }
